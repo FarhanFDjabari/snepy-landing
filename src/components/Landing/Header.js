c41d750cf1438ed488ba2d54c604adbe8e8303fb
Ramdani Kurnia <adiatma85@gmail.com>
2021-08-17 21:57:26 +0700

    Refix navbar, description, and integration

--- src/components/Landing/Header.js
+++ src/components/Landing/Header.js
@@ -8,7 +8,7 @@ function Header() {
           <h1 className="jumbotron__text jumbotron__text__main jumbotron__text__color-white">Bantal Leher <br/> Jaman Now Paling Hit</h1>
           <h2 className="jumbotron__text jumbotron__text__color-white mt-5">Pegal dan kaku leher hilang dalam 5 menit</h2>
           <div className="d-flex justify-content-end pt-5">
-            <div className="btn-buy jumbotron__text__btn-buy jumbotron__text__color-white me-4">Beli Sekarang</div>
+            <div className="btn-buy jumbotron__text__btn-buy jumbotron__text__color-white me-4">Pre-Order Sekarang</div>
             <a className="btn-buy learn-more jumbotron__text__color-white jumbotron__text__btn-buy" href="https://google.com">Pelajari lebih</a>
           </div>
         <div>
--- src/components/Landing/Integration.js
+++ src/components/Landing/Integration.js
@@ -1,5 +1,5 @@
 import "./Integration.scss"
-import { Container, Row, Col } from "react-bootstrap"
+import { Row, Col } from "react-bootstrap"
 import AppStoreLogo from "../../asset/img/appstore-btn.png"
 import PlayStoreLogo from "../../asset/img/playstore-btn.png"
 import ImgIntegration from "../../asset/img/img-integration.svg"
@@ -7,7 +7,7 @@ import ImgIntegration from "../../asset/img/img-integration.svg"
 function Integration() {
     return (
         <>
-            <Container className="integration-container">
+            <div className="container-fluid integration-container">
                 <Row className="px-5">
                     <Col className="d-flex flex-column row-integration">
                         <div className="content integration-container__text">
@@ -19,14 +19,11 @@ function Integration() {
                             <img src={PlayStoreLogo} alt="" className="mx-4" />
                         </div>
                     </Col>
-                    <Col className="d-flex justify-content-end">
-                        {/* For Md to large */}
-                        <img src={ImgIntegration} alt="" className="d-none d-md-block" />
-                        {/* For Small Only */}
-                        <img src={ImgIntegration} alt="" className="d-block d-md-none responsive__phone-image" />
+                    <Col className="d-flex justify-content-md-start justify-content-center justify-content-lg-end">
+                        <img src={ImgIntegration} alt="" className="responsive__phone-image" />
                     </Col>
                 </Row>
-            </Container>
+            </div>
         </>
     )
 }
